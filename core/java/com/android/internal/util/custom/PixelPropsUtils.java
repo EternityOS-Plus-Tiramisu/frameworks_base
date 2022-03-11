d1446767dd0d7fb9b13c130d54b432de1ed4be15
Joey Huab <joey@evolution-x.org>
2022-03-11 16:26:30 +0900

    PixelPropsUtils: Spoof Snapchat
    * For Night Sight
    
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -58,6 +58,7 @@ public class PixelPropsUtils {
             "com.android.chrome",
             "com.android.vending",
             "com.breel.wallpapers20",
+	    "com.snapchat.android",
             "com.amazon.avod.thirdpartyclient",
             "com.disney.disneyplus",
             "com.netflix.mediaclient",
